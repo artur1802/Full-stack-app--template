addef1008f49098abee2497d986d53a94e378d02
Arturo Rincon <artur18_02@hotmail.com>
2022-06-23 20:16:09 -0500

    add index. ejs style.css

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ MongoClient.connect(dbConnectionString)
     collection = db.collection('alien-info')
 })
 
+app.set('view engine', 'ejs')
+
+//it lets our app in heroku or whereever it lets to serve us some file in public when we call it
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+// help express parse our data in  json 
+app.use(express.json())
+app.use(cors())
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`server is running on port `)
